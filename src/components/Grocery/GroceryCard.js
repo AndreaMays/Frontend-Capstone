bd398cfa852847c6cba65b1520c2cd17801c7344
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-03-16 22:14:54 -0500

    Grocery Card(s) rendering to DOM with list of groceries in each card as well

--- src/components/Grocery/GroceryCard.js
+++ src/components/Grocery/GroceryCard.js
@@ -2,47 +2,81 @@ import { Card } from "react-bootstrap"
 import { CardDeck } from "react-bootstrap"
 
 export const GroceryCard = () =>  (
-
+<>
 <CardDeck>
   <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Img variant="top" src="Immediate-Relief-Starts-with-Food-Featured-Image-1400x716.png.webp"/> 
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
+      <Card.Title>Groceries Items Week 1</Card.Title>
       <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
+        Grocery Items: 
+        <li>1 Jar of Peanut Butter </li>
+        <li>1 Jar of Jelly </li>
+        <li>1 Box of Captin Crunch Cereal </li>
+        <li>3 Small Boxes of Milk </li>
+        <li>2 Large Cans of Chicken </li>
+        <li>1 Package of Hotdogs </li>
+        <li>1 package of Celery </li>
+            
       </Card.Text>
     </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
   </Card>
+
   <Card>
     <Card.Img variant="top" src="holder.js/100px160" />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
+      <Card.Title>Grocery Items Week 2</Card.Title>
       <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
+      Grocery Items: 
+        <li>2 Cans of Tuna </li>
+        <li>1 Bag Dry Black Beans </li>
+        <li>1lb Dry White Rice </li>
+        <li>1lb Dry Oatmeal </li>
+        <li>Breakfast Bars </li>
+        <li>2 Boxes of Mac and Cheese</li>
+        <li>1lb of Carrots</li>
       </Card.Text>
     </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
   </Card>
+
   <Card>
     <Card.Img variant="top" src="holder.js/100px160" />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
+      <Card.Title>Grocery Items Week 3</Card.Title>
       <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
+      Grocery Items: 
+        <li>1 Box Dry Spaghetti</li>
+        <li>1 Jar Spaghetti Sauce </li>
+        <li>1 Box of Cake Mix </li>
+        <li>1 Box of Cheerios </li>
+        <li>3 Small boxes Chocolate Milk</li>
+        <li>2 Cans mixed fruit</li>
+        <li>2 Cans Beef Stew</li>
       </Card.Text>
     </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
   </Card>
+
+<Card>
+  <Card.Img variant="top" src="holder.js/100px160" />
+  <Card.Body>
+    <Card.Title>Grocery Items Week 4</Card.Title>
+    <Card.Text>
+    Grocery Items: 
+      <li>1 Gallon Milk</li>
+      <li>Frozen Chicken Legs</li>
+      <li>1lb of Onions</li>
+      <li>5lbs Potatoes </li>
+      <li>1lb of Cheese</li>
+      <li>2lb bag of Apples</li>
+      <li>1 pint of Strawberry Yogurt</li>
+      <li>1 pint of Sour Cream</li>
+
+    </Card.Text>
+  </Card.Body>
+</Card>
 </CardDeck>
+</>
 )
+
+
+
